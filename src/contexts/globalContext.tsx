8ad4eb92e384330e28562cc97814a55184f15de2
evr.onen <evr.onengmail.com>
2023-03-01 11:47:10 +0300

    Authorization complate

--- src/contexts/globalContext.tsx
+++ src/contexts/globalContext.tsx
@@ -1,30 +1,68 @@
-import React, { useState } from "react"
-
+// ** React / Next Core
+import React, { useEffect, useState } from "react"
+import Axios from "axios"
+import Router from "next/router"
+import jwt_decode from "jwt-decode"
+import { DecodedType } from "@/types/response"
+// ** Types
+type UserType = { name: string; surname: string; userRole: number; storeId: number; token: null | string }
 interface IGlobalContextProps {
-  user: string
+  user: UserType
   settings: Settings
-  setUser: (user: string) => void
+  setUser: (user: UserType) => void
   setSettings: (themeMode: Settings) => void
 }
 
-// ** Types
 type Settings = {
   mode: string
   lang: string
   direction: string
 }
-
 export const GlobalContext = React.createContext<IGlobalContextProps>({
-  user: "",
+  user: { name: "", surname: "", userRole: 0, storeId: 0, token: null },
   settings: { mode: "light", lang: "tr", direction: "ltr" },
   setUser: () => {},
   setSettings: () => {},
 })
 
 export const GlobalContextProvider = (props: any) => {
-  const [currentUser, setCurrentUser] = useState("")
   const [themeSettings, setthemeSettings] = useState({ mode: "light", lang: "tr", direction: "ltr" })
+  const [currentUser, setCurrentUser] = useState<UserType>({ name: "", surname: "", userRole: 0, storeId: 0, token: null })
 
+  useEffect(() => {
+    if (currentUser.token === null) {
+      let token = localStorage?.user
+
+      if (token !== undefined) {
+        const ttt = async () => {
+          try {
+            const response = await Axios.post("http://127.0.0.1:8000/api/auth/refresh", null, {
+              headers: {
+                "Content-type": "application/json",
+                Authorization: `Bearer ${token}`,
+              },
+            })
+            console.log(response)
+            const decoded: DecodedType = jwt_decode(response?.data?.access_token)
+            setCurrentUser({
+              name: decoded.name,
+              surname: decoded.surn,
+              userRole: decoded.role,
+              storeId: decoded.stor,
+              token: response?.data?.access_token,
+            })
+          } catch (response) {
+            Router.push("/login")
+          }
+        }
+        ttt()
+      } else {
+        Router.push("/login")
+      }
+    } else {
+      console.log(currentUser)
+    }
+  }, [])
   return (
     <GlobalContext.Provider
       value={{
@@ -38,3 +76,7 @@ export const GlobalContextProvider = (props: any) => {
     </GlobalContext.Provider>
   )
 }
+
+// ** bunu ne zaman yapacak
+// ** eger context bos ise local storageden token i kullanarak refresh yapacak
+// ** eger context dolu ise es gececek
---
+++ src/types/response.ts
@@ -0,0 +1,14 @@
+export type DecodedType = {
+  exp: number
+  iat: number
+  iss: string
+  jti: string
+  mail: string
+  name: string
+  nbf: number
+  prv: string
+  role: number
+  stor: number
+  sub: string
+  surn: string
+}
